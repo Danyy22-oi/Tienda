716a52a6218bd97f59a486ed2d38ed5ef432573a
Danyy22-oi <tdanielaaa22@gmail.com>
2023-06-28 01:06:22 -0600

    service semana 5

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,17 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+
+    public void delete(Categoria categoria);
+
+    public Categoria getCategoria(Categoria categoria);
+
+    public void save(Categoria categoria);
+
+}
