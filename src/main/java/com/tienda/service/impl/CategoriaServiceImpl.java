86cab8cd5181589b8c07a7ea4ac5af7aa1f1939c
Danyy22-oi <tdanielaaa22@gmail.com>
2023-07-24 17:26:57 -0600

    arreglos

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
@@ -24,25 +28,21 @@ public class CategoriaServiceImpl implements CategoriaService {
         return lista;
     }
 
+    @Override
     @Transactional(readOnly = true)
-    public Categoria getC(Categoria categoria) {
+    public Categoria getCategoria(Categoria categoria) {
         return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
     }
 
-    @Transactional
     @Override
+    @Transactional
     public void save(Categoria categoria) {
         categoriaDao.save(categoria);
     }
 
-    @Transactional
     @Override
+    @Transactional
     public void delete(Categoria categoria) {
         categoriaDao.delete(categoria);
-    }
-
-    @Override
-    public Categoria getCategoria(Categoria categoria) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
